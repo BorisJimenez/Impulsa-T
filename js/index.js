1ab34c8aa51b336129cbbacd206cefd891d3bc07
Boris <borismjimenezj@gmail.com>
2025-07-11 22:43:07 -0400

    proyecto terminado

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
+import scrollSpy from "./scroll_espia.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
+  scrollSpy("");
 });
